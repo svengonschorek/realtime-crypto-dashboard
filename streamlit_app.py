ccab1dc6c2af95665d25e24f02edf3e1bf40c61d
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2025-10-24 10:28:56 +0200

    master: extend timeline for candlestick chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,14 +58,30 @@ with st.container(horizontal_alignment="left"):
 if timeframe_switch is not None:
     interval = map_timeframe_to_interval(option_map[timeframe_switch])
 
+def data_loader():
+    # load base data
+    base_data = pd.DataFrame(get_data("SOL", "USDT", interval)).sort_values(by='time')
+
+    # extend time series into the future
+    max_time = pd.to_datetime(base_data['time'].max())
+    start_time = max_time + pd.Timedelta(minutes=int(interval) if interval not in ['D'] else 1440)
+    future_time_series = pd.date_range(
+        start=start_time,
+        periods=100,
+        freq=f"{interval}min" if interval not in ['D'] else 'D')
+    extend_time = pd.DataFrame({'time': future_time_series})
+
+    # combine base data with extended time series
+    return pd.concat([base_data, extend_time], ignore_index=True)
+
 with st.container(horizontal_alignment="center"):
     chart = StreamlitChart(
         height=screen_stats['innerHeight'] * 0.7,
-        width=screen_stats['innerWidth'] * 0.95,
+        width=screen_stats['innerWidth'] * 0.95
     )
 
     chart.layout(background_color="#0a0a0a", text_color="#ffffff")
     chart.grid(vert_enabled=False, horz_enabled=False)
 
-    chart.set(pd.DataFrame(get_data("SOL", "USDT", interval)).sort_values(by='time'))
+    chart.set(data_loader())
     chart.load()
