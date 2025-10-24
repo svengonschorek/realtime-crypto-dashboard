2b6795a693e5bf1f343c88131249c1d8d8257e9c
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2025-10-24 20:14:20 +0200

    master: put candlestick chart to components

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 import os, sys
 import streamlit as st
-import pandas as pd
 
-from lightweight_charts.widgets import StreamlitChart
 from st_screen_stats import ScreenData
 
 # Add the project root to the Python path
@@ -11,6 +9,7 @@ project_root = os.path.join(script_dir, "..")
 sys.path.append(os.path.abspath(project_root))
 
 from src.api.bybit.history_data import get_data
+from src.components.charts.candlestick_chart import candlestick_chart
 
 screenD = ScreenData(setTimeout=1000)
 screen_stats = screenD.st_screen_data()
@@ -27,67 +26,7 @@ st.set_page_config(layout="wide")
 st.title("Crypto Trading Analysis Dashboard")
 st.write("")
 
-interval = "5"
-
-# Data Preparation
-def map_timeframe_to_interval(timeframe: str) -> str:
-    mapping = {
-        '1m': '1',
-        '5m': '5',
-        '1h': '60',
-        '4h': '240',
-        '1d': 'D'
-    }
-    return mapping.get(timeframe, '5')
-
-
-with st.container(horizontal_alignment="left"):
-
-    option_map = {
-        0: "1m",
-        1: "5m",
-        2: "1h",
-        3: "4h",
-        4: "1d"
-    }
-
-    timeframe_switch = st.segmented_control(
-        default=1,
-        label="Timeframe",
-        label_visibility="collapsed",
-        options=option_map.keys(),
-        format_func=lambda option: option_map[option],
-        selection_mode="single",
-    )
-
-# Update interval based on selection
-if timeframe_switch is not None:
-    interval = map_timeframe_to_interval(option_map[timeframe_switch])
-
-def data_loader():
-    # load base data
-    base_data = pd.DataFrame(get_data("SOL", "USDT", interval)).sort_values(by='time')
-
-    # extend time series into the future
-    max_time = pd.to_datetime(base_data['time'].max())
-    start_time = max_time + pd.Timedelta(minutes=int(interval) if interval not in ['D'] else 1440)
-    future_time_series = pd.date_range(
-        start=start_time,
-        periods=100,
-        freq=f"{interval}min" if interval not in ['D'] else 'D')
-    extend_time = pd.DataFrame({'time': future_time_series})
-
-    # combine base data with extended time series
-    return pd.concat([base_data, extend_time], ignore_index=True)
-
-with st.container(horizontal_alignment="center"):
-    chart = StreamlitChart(
-        height=screen_stats['innerHeight'] * 0.7,
-        width=screen_stats['innerWidth'] * 0.95
-    )
-
-    chart.layout(background_color="#0a0a0a", text_color="#ffffff")
-    chart.grid(vert_enabled=False, horz_enabled=False)
-
-    chart.set(data_loader())
-    chart.load()
+candlestick_chart(
+    height=screen_stats['innerHeight'] * 0.65,
+    width=screen_stats['innerWidth'] * 0.95
+)
---
+++ src/components/charts/candlestick_chart.py
@@ -0,0 +1,78 @@
+import os, sys
+import streamlit as st
+import pandas as pd
+
+from lightweight_charts.widgets import StreamlitChart
+
+# Add the project root to the Python path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.join(script_dir, "..", "..", "..")
+sys.path.append(os.path.abspath(project_root))
+
+from src.api.bybit.history_data import get_data
+
+def candlestick_chart(height, width):
+    interval = "5"
+
+    # Data Preparation
+    def map_timeframe_to_interval(timeframe: str) -> str:
+        mapping = {
+            '1m': '1',
+            '5m': '5',
+            '1h': '60',
+            '4h': '240',
+            '1d': 'D'
+        }
+        return mapping.get(timeframe, '5')
+
+
+    with st.container(horizontal_alignment="left"):
+
+        option_map = {
+            0: "1m",
+            1: "5m",
+            2: "1h",
+            3: "4h",
+            4: "1d"
+        }
+
+        timeframe_switch = st.segmented_control(
+            default=1,
+            label="Timeframe",
+            label_visibility="collapsed",
+            options=option_map.keys(),
+            format_func=lambda option: option_map[option],
+            selection_mode="single",
+        )
+
+    # Update interval based on selection
+    if timeframe_switch is not None:
+        interval = map_timeframe_to_interval(option_map[timeframe_switch])
+
+    def data_loader():
+        # load base data
+        base_data = pd.DataFrame(get_data("SOL", "USDT", interval)).sort_values(by='time')
+
+        # extend time series into the future
+        max_time = pd.to_datetime(base_data['time'].max())
+        start_time = max_time + pd.Timedelta(minutes=int(interval) if interval not in ['D'] else 1440)
+        future_time_series = pd.date_range(
+            start=start_time,
+            periods=100,
+            freq=f"{interval}min" if interval not in ['D'] else 'D')
+        extend_time = pd.DataFrame({'time': future_time_series})
+
+        # combine base data with extended time series
+        return pd.concat([base_data, extend_time], ignore_index=True)
+
+    with st.container(horizontal_alignment="center"):
+        chart = StreamlitChart(
+            height=height,
+            width=width
+        )
+
+        chart.layout(background_color="#0a0a0a", text_color="#ffffff")
+        chart.grid(vert_enabled=False, horz_enabled=False)
+
+        chart.set(data_loader())
+        chart.load()
